f2c7ce9c699f5286e1ff52dcd5055f4652bce7b2
J3ff5 <jeffersonsoaresbh@gmail.com>
2022-08-03 18:05:28 -0300

    Initial Commit

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Qesh Developer on 03/08/22.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
